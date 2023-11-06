f98d872851ec971c9b71a925cdcc1e9ba93d3412
pabloandrei <pabloandrei0906@gmailcom.br>
2023-11-05 22:29:32 -0300

    inicialização do form

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -1,85 +1,50 @@
-<script setup lang="ts">
-import { RouterLink, RouterView } from 'vue-router'
-import HelloWorld from './components/HelloWorld.vue'
-</script>
-
 <template>
-  <header>
-    <img alt="Vue logo" class="logo" src="@/assets/logo.svg" width="125" height="125" />
+  <main>
+    <form>
+      <div class="inputs">
+        <label for="name">Nome</label>
+        <input type="text" placeholder="Nome" id="nome">
+      </div>
+      <div class="inputs">
+        <label for="age">Idade</label>
+        <input type="number" placeholder="Idade">
+      </div>
+      <div class="inputs">
+        <label for="sex">Sexo</label>
+        <select name="sexo" id="sexo">
+          <option value="M">M</option>
+          <option value="F">F</option>
+        </select>
+      </div>
+      <div class="inputs">
+        <textarea name="inventario" id="itens" cols="30" rows="5"></textarea>
+      </div>
 
-    <div class="wrapper">
-      <HelloWorld msg="You did it!" />
-
-      <nav>
-        <RouterLink to="/">Home</RouterLink>
-        <RouterLink to="/about">About</RouterLink>
-      </nav>
-    </div>
-  </header>
-
-  <RouterView />
+      <Button />
+    </form>
+  </main>
 </template>
 
-<style scoped>
-header {
-  line-height: 1.5;
-  max-height: 100vh;
-}
+<script>
+import Button from './components/Button.vue'
 
-.logo {
-  display: block;
-  margin: 0 auto 2rem;
+export default {
+  name: 'App',
+  components: {
+    Button
+  }
 }
-
-nav {
-  width: 100%;
-  font-size: 12px;
+</script>
+<style>
+form{
+  background-image: url('./assets/img/fundo.jpg');
+  width: 20vw;
+  height: 40vh;
   text-align: center;
-  margin-top: 2rem;
+  padding: 10%;
 }
 
-nav a.router-link-exact-active {
-  color: var(--color-text);
-}
-
-nav a.router-link-exact-active:hover {
-  background-color: transparent;
-}
-
-nav a {
-  display: inline-block;
-  padding: 0 1rem;
-  border-left: 1px solid var(--color-border);
-}
-
-nav a:first-of-type {
-  border: 0;
-}
-
-@media (min-width: 1024px) {
-  header {
-    display: flex;
-    place-items: center;
-    padding-right: calc(var(--section-gap) / 2);
-  }
-
-  .logo {
-    margin: 0 2rem 0 0;
-  }
-
-  header .wrapper {
-    display: flex;
-    place-items: flex-start;
-    flex-wrap: wrap;
-  }
-
-  nav {
-    text-align: left;
-    margin-left: -1rem;
-    font-size: 1rem;
-
-    padding: 1rem 0;
-    margin-top: 1rem;
-  }
+.inputs {
+  margin-bottom: 4%;
 }
 </style>
---
+++ frontend/src/components/button.vue
@@ -0,0 +1,19 @@
+<template>
+    <button>Cadastrar</button>
+</template>
+
+<script>
+    export default {
+        name: 'Button'
+    }
+</script>
+
+<style>
+    button {
+        width: 200px;
+        height: 40px;
+        font-size: 20px;
+        background-color: aqua;
+        border-radius: 5%;
+    }
+</style>
