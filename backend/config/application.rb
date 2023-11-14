37dbe6fe67f6476edbc2a06f01d9da97365def3c
Adnir Andrade <adnir_andrade@hotmail.com>
2023-11-14 19:35:52 -0300

    Enable CORS in the backend

--- backend/config/application.rb
+++ backend/config/application.rb
@@ -8,6 +8,13 @@ Bundler.require(*Rails.groups)
 
 module Backend
   class Application < Rails::Application
+    config.middleware.insert_before 0, Rack::Cors do
+      allow do
+        origins 'http://localhost:5173' # Add your Vue.js app's origin
+        resource '*', headers: :any, methods: [:get, :post, :put, :patch, :delete, :options, :head]
+      end
+    end
+    
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 7.1
 
