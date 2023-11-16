f2ddfa23dbaa3b07c8ed063d12b5b477061359b2
adnir-andrade <adnir_andrade@hotmail.com>
2023-11-16 13:51:04 -0300

    Fix texture bug

--- frontend/src/components/CustomButton.vue
+++ frontend/src/components/CustomButton.vue
@@ -1,6 +1,6 @@
 <template>
   <button
-    class="w-[163px] bg-amber-100 h-10 text-xl text-[#110102] rounded-[10%] bg-[url('.\img\fundo-button.png')] bg-cover bg-[right_-63px_bottom_-30px]"
+    class="w-[163px] bg-amber-100 h-10 text-xl text-[#110102] rounded-[10%] bg-[url('src/assets/img/fundo-button.png')] bg-cover bg-[right_-63px_bottom_-30px]"
   >
     Cadastrar
   </button>
