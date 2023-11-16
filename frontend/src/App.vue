0cabdd9c04d277ea802962f9ce98d0343506d871
Adnir Andrade <adnir_andrade@hotmail.com>
2023-11-16 09:35:13 -0300

    Refactor form inside component

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -1,62 +1,13 @@
+<script setup lang="ts">
+import Form from './components/Form.vue';
+</script>
+
 <template>
   <main>
-    <form>
-      <div class="inputs">
-        <label for="name">Nome</label>
-        <input type="text" placeholder="Nome" id="nome" />
-      </div>
-      <div class="inputs">
-        <label for="age">Idade</label>
-        <input type="number" placeholder="Idade" />
-      </div>
-      <div class="inputs">
-        <label for="sex">Sexo</label>
-        <select name="sexo" id="sexo">
-          <option value="M">M</option>
-          <option value="F">F</option>
-        </select>
-      </div>
-      <div class="posicao">
-        <h3>Ultima Posição</h3>
-        <div class="inputs">
-          <label for="longitude">Longitude</label>
-          <input type="text" placeholder="52º 03 21 W" />
-        </div>
-        <div class="inputs">
-          <label for="latitude">Latitude</label>
-          <input type="text" placeholder=" 25º 34 20 S" />
-        </div>
-      </div>
-      <div class="inputs">
-        <textarea name="inventario" id="itens" cols="30" rows="5"></textarea>
-      </div>
-      <Button @click="getData" />
-    </form>
+    <Form></Form>
   </main>
 </template>
 
-<script>
-import Button from './components/Button.vue';
-import api from './api.ts';
-
-export default {
-  name: 'App',
-  components: {
-    Button,
-  },
-  methods: {
-    async getData(event) {
-      event.preventDefault();
-      try {
-        const response = await api.getData();
-        console.log(response.data); // Handle the response data as needed
-      } catch (error) {
-        console.error('Error fetching data:', error);
-      }
-    },
-  },
-};
-</script>
 <style>
 @import url('https://fonts.googleapis.com/css2?family=Island+Moments&family=Montserrat:ital,wght@1,300&family=Nothing+You+Could+Do&family=Noto+Sans:wght@400;700&family=Palanquin+Dark:wght@400;500;600&display=swap');
 main {
---
+++ frontend/src/components/Form.vue
@@ -0,0 +1,56 @@
+<script lang="ts">
+import Button from './Button.vue';
+import api from '../api';
+
+export default {
+  components: {
+    Button,
+  },
+  methods: {
+    async getData() {
+      console.log('Clicked!');
+      try {
+        const response = await api.getData();
+        console.log(response.data);
+      } catch (error) {
+        console.error('Error fetching data:', error);
+      }
+    },
+  },
+};
+</script>
+
+<template>
+  <form>
+    <div class="inputs">
+      <label for="name">Nome</label>
+      <input type="text" placeholder="Nome" id="nome" />
+    </div>
+    <div class="inputs">
+      <label for="age">Idade</label>
+      <input type="number" placeholder="Idade" />
+    </div>
+    <div class="inputs">
+      <label for="sex">Sexo</label>
+      <select name="sexo" id="sexo">
+        <option value="M">M</option>
+        <option value="F">F</option>
+      </select>
+    </div>
+    <div class="posicao">
+      <h3>Ultima Posição</h3>
+      <div class="inputs">
+        <label for="longitude">Longitude</label>
+        <input type="text" placeholder="52º 03 21 W" />
+      </div>
+      <div class="inputs">
+        <label for="latitude">Latitude</label>
+        <input type="text" placeholder=" 25º 34 20 S" />
+      </div>
+    </div>
+    <div class="inputs">
+      <textarea name="inventario" id="itens" cols="30" rows="5"></textarea>
+    </div>
+    <Button @click.prevent="getData" />
+  </form>
+</template>
