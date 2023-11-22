b31a724116c70009d810527c899e8fa613dbe7ad
Adnir Andrade <adnir_andrade@hotmail.com>
2023-11-22 07:40:49 -0300

    Refactor getFullSurvivor to getSurvivor index

--- backend/config/routes.rb
+++ backend/config/routes.rb
@@ -1,7 +1,5 @@
 Rails.application.routes.draw do
   resources :survivors do
-    get '/full_survivors', to: 'survivors#full_survivor', as: 'survivors_complete'
-    get '/locations/full_survivor', to: 'locations#full_survivor', as: 'full_info'
     resources :locations
   end
 
